04e3aac55a21c79a18e7d6f7f652282a889022c1
Foxik <Foxik@mail.ru>
2023-03-28 19:49:15 +0300

    lab09

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите количество векторов:");
+int vector = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество вершин:");
+int top = Convert.ToInt32(Console.ReadLine());
+Dictionary <int, List<int>> dictionary = new Dictionary<int, List<int>>();
+for (int i = 1; i < vector + 1; i++) { dictionary.Add(i, new List<int>()); }
+Console.WriteLine("Соедините вершины через ENTER");
+for(int i = 0; i < top; i++)
+{
+    int top1 = Convert.ToInt32(Console.ReadLine());; int top2 = Convert.ToInt32(Console.ReadLine());
+    dictionary[top1].Add(top2);
+    dictionary[top2].Add(top1);
+}
+foreach (int consul in dictionary.Keys)
+{ Console.WriteLine($"{consul}:" + String.Join(" ", dictionary[consul])); }
+
